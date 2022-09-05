4aa5abac1140afc71201a03a69cbf727df9be3e8
YuHan Ma <59942793+orangutanz@users.noreply.github.com>
2022-09-05 14:22:31 -0700

    Change player pawn

--- Source/Multiplayer_Project/Multiplayer_ProjectGameMode.cpp
+++ Source/Multiplayer_Project/Multiplayer_ProjectGameMode.cpp
@@ -11,14 +11,14 @@ AMultiplayer_ProjectGameMode::AMultiplayer_ProjectGameMode()
 	PlayerControllerClass = AMultiplayer_ProjectPlayerController::StaticClass();
 
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Character/Blueprints/BP_TopDownCharacter"));
 	if (PlayerPawnBPClass.Class != nullptr)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
 
 	// set default controller to our Blueprinted controller
-	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownPlayerController"));
+	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/Character/Blueprints/BP_TopDownController"));
 	if(PlayerControllerBPClass.Class != NULL)
 	{
 		PlayerControllerClass = PlayerControllerBPClass.Class;
